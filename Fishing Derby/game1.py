ca40ab16ef770d1d6378da56ce3f0012fa62047a
sribhargav1345 <sribhargav1345@gmail.com>
2023-11-18 14:48:59 +0530

    Day-2

--- Fishing Derby/game1.py
+++ Fishing Derby/game1.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # Initialize pygame
 pygame.init()
@@ -83,7 +84,44 @@ background_image = pygame.image.load("background-2.jpg")
 background_image = pygame.transform.scale(background_image, (800,600))
 
 score = 0
-time_limit = 30
+time_limit = 5
+
+class Hook:
+    def __init__(self):
+        
+        self.anchor_x = 100     # Fixed end
+        self.anchor_y = 400
+
+        self.end_x = self.anchor_x          # Movable end
+        self.end_y = self.anchor_y - 100
+
+        self.length = 100                   #Min.length
+
+        self.color = BLACK
+
+    def update(self, direction):
+        # Adjust the hook's length based on the direction
+        if direction == "right":
+            self.length += random.randint(1,150)
+        elif direction == "left":
+            self.length -= random.randint(1,150)
+        elif direction == "up":
+            self.end_y = self.anchor_y - random.randint(1,150)
+        elif direction == "down":
+            self.end_y = self.anchor_y + random.randint(1,150)
+
+        # Limit the hook's length to a minimum of 10
+        if self.length < 10:
+            self.length = 10
+
+        # Update the hook's end point coordinates
+        self.end_x = self.anchor_x + (self.length * math.cos(math.radians(90 + direction)))
+        self.end_y = self.anchor_y + (self.length * math.sin(math.radians(90 + direction)))
+
+    def draw(self, screen):
+        # Draw the hook line
+        pygame.draw.line(screen, self.color, (self.anchor_x, self.anchor_y), (self.end_x, self.end_y), 3)
+
 
 # Main game loop
 running = True
@@ -95,77 +133,69 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                fish2_x -= fish2_speed
-            if event.key == pygame.K_RIGHT:
-                fish2_x += fish2_speed
-            if event.key == pygame.K_UP:
-                fish2_y -= fish2_speed
-            if event.key == pygame.K_DOWN:
-                fish2_y += fish2_speed
+    # Calculate remaining time
+    remaining_time = max(0, time_limit - (pygame.time.get_ticks() - start_time) / 1000)
 
-    #screen.blit(background_image, (0, 200))
-    remaining_time = time_limit - (pygame.time.get_ticks() - start_time) / 1000
+    if remaining_time > 0:
+        # Update the fish position
+        for fish in fishes:
+            fish["x"] += fish["speed"] * fish["direction"]
+            if fish["x"] > 800:
+                fishes.remove(fish)  
+            elif fish["x"] < -15:
+                fishes.remove(fish)
 
-    # Update the fish position
-    for fish in fishes:
-        fish["x"] += fish["speed"] * fish["direction"]
-        if fish["x"] > 800:
-            fishes.remove(fish)  
-        elif fish["x"] < -15:
-            fishes.remove(fish)
+        for fish in fishes2:
+            fish["x"] += fish["speed"] * fish["direction"]
+            if fish["x"] > 800:
+                fishes2.remove(fish) 
+            elif fish["x"] < -15:
+                fishes2.remove(fish)
 
-    for fish in fishes2:
-        fish["x"] += fish["speed"] * fish["direction"]
-        if fish["x"] > 800:
-            fishes2.remove(fish) 
-        elif fish["x"] < -15:
-            fishes2.remove(fish)
+        # Generate new fishes
+        if len(fishes) < 10:
+            fish_y = random.randint(400, 600)
+            fish_direction = random.choice([-1, 1])
+            if fish_direction == -1:
+                fish_x = 800
+                fishes.append({
+                    "x": fish_x,
+                    "y": fish_y,
+                    "speed": fish_speed,
+                    "direction": fish_direction,
+                    "image": fishr_image
+                })
+            else:
+                fish_x = 0
+                fishes.append({
+                    "x": fish_x,
+                    "y": fish_y,
+                    "speed": fish_speed,
+                    "direction": fish_direction,
+                    "image": fish_image
+                })
 
-    if remaining_time > 0 and len(fishes) < 10:
-        fish_y = random.randint(400, 600)
-        fish_direction = random.choice([-1, 1])
-        if(fish_direction == -1):
-            fish_x = 800
-            fishes.append({
-            "x": fish_x,
-            "y": fish_y,
-            "speed": fish_speed,
-            "direction": fish_direction,
-            "image": fishr_image
-        })
-        else:
-            fish_x = 0
-            fishes.append({
-            "x": fish_x,
-            "y": fish_y,
-            "speed": fish_speed,
-            "direction": fish_direction,
-            "image": fish_image
-        })
-    
-    if remaining_time > 0 and len(fishes2) < 10:
-        fish2_y = random.randint(400, 600)
-        fish2_direction = random.choice([-1, 1])
-        if(fish2_direction == -1):
-            fish2_x = 800
-            fishes2.append({
-            "x": fish2_x,
-            "y": fish2_y,
-            "speed": fish2_speed,
-            "direction": fish2_direction,
-            "image": fish2r_image
-        })
-        else:
-            fish2_x = 0
-            fishes2.append({
-            "x": fish2_x,
-            "y": fish2_y,
-            "speed": fish2_speed,
-            "direction": fish2_direction,
-            "image": fish2_image
-        })
+        if len(fishes2) < 10:
+            fish2_y = random.randint(400, 600)
+            fish2_direction = random.choice([-1, 1])
+            if fish2_direction == -1:
+                fish2_x = 800
+                fishes2.append({
+                    "x": fish2_x,
+                    "y": fish2_y,
+                    "speed": fish2_speed,
+                    "direction": fish2_direction,
+                    "image": fish2r_image
+                })
+            else:
+                fish2_x = 0
+                fishes2.append({
+                    "x": fish2_x,
+                    "y": fish2_y,
+                    "speed": fish2_speed,
+                    "direction": fish2_direction,
+                    "image": fish2_image
+                })
 
     # Check for a collision
     if abs(fish_x - fish2_x) < 50 and abs(fish_y - fish2_y) < 50:
@@ -174,26 +204,43 @@ while running:
         fish_y = random.randint(400, 600)
 
     # Fill the screen
-    screen.fill(GREEN)
+    #screen.fill(GREEN)
     screen.blit(background_image, (0, 0))
 
+    # Draw fishes
     for fish in fishes:
-        if(fish["direction"]==1):
+        if fish["direction"] == 1:
             screen.blit(fish_image, (fish["x"], fish["y"]))
         else:
             screen.blit(fishr_image, (fish["x"], fish["y"]))
     for fish in fishes2:
-        if(fish["direction"]==1):
+        if fish["direction"] == 1:
             screen.blit(fish2_image, (fish["x"], fish["y"]))
         else:
             screen.blit(fish2r_image, (fish["x"], fish["y"]))
 
+    # Display score and time
     font = pygame.font.SysFont("Arial", 32)
-    score_text = font.render(f"Score: {score}", True, WHITE)
-    time_text = font.render(f"Time Remaining: {int(remaining_time)}", True, WHITE)
+    score_text = font.render(f"Score: {score}", True, BLACK)
+    time_text = font.render(f"Time: {int(remaining_time)}", True, BLACK)
+
     screen.blit(score_text, (10, 10))
+    screen.blit(time_text, (370, 10))
+    screen.blit(score_text, (680, 10))
 
     # Update the display
     pygame.display.flip()
+
+    # Check if the game should end
+    if remaining_time == 0:
+        running = False
+
+# Display final score
+final_score_text = font.render(f"Final Score: {score}", True, RED)
+screen.blit(final_score_text, (300, 300))
+pygame.display.flip()
+
+# Wait for a few seconds before quitting
+pygame.time.wait(5000)
 
 pygame.quit()
